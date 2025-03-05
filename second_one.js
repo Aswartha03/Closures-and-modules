5cb06e829a4262f81117911c74ad3791f9affe11
Aswarth <212g1a0403@gmail.com>
2025-03-05 18:58:04 +0530

    firstJSON

---
+++ second_one.js
@@ -0,0 +1,11 @@
+function JStoJSON(obj){
+    console.log(JSON.stringify(obj))
+}
+let student = {
+    name: "Alice",
+    age: 22,
+    course: "Computer Science"
+  };
+  
+JStoJSON(student)
+// }
